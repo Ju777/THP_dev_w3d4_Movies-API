0f2140e3eda149024a9670a6a65874a1d68349fd
Julien Armagnac <julien.tamil@gmail.com>
2022-07-20 12:10:22 +0200

    Add link with API, it is working and displaying raw data. Needs design.

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,59 @@
-// l'url pour fetch a été trouvée ici (?)
-var url = 'https://api.weatherbit.io/v2.0/current?&city=Anchorage&key=df1ba2df7a47434abe4fef3dd726212a&include=minutely';
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+
+// var url = 'http://www.omdbapi.com/?s=prout&apikey=1903931a';
 
 try {
     getData = async (url) => {
         const response = await fetch(url);
         const data = await response.json();
-        // console.log(data);
+        console.log(data);
         return data;
     }
 } catch(error) {
     console.log("Erreur dans le fetch => " + error);
 }
 
+getInput = () => {
+    
+    searchButton.addEventListener('click', async () => {
+        console.clear();
+        console.log(searchInput.value);
+        let url = inputToURL(searchInput.value);
+        let data = await getData(url);
+        displayResults(data);
+    });
+}
 
+inputToURL = (input) => {
+    console.log('http://www.omdbapi.com/?s=' + input + '&apikey=1903931a')
+    return 'http://www.omdbapi.com/?s=' + input + '&apikey=1903931a'
+}
+
+displayResults = (data) => {
+    // log de vérif
+    // console.log("on est dans displayResults, la paramètre est");
+    // console.log(data.Search[0]);
+    let results = data.Search;
+    let html = '';
+
+    results.map(movie => {
+        let htmlSegment =
+            `
+            <div>${movie.Title}</div>
+            `
+        html += htmlSegment;
+    });
+
+    const dataContainer = document.getElementById('data-container');
+    dataContainer.innerHTML = html;
+
+
+}
 
 perform = () => {
-
+  
+    getInput();
 }
 
 perform();
